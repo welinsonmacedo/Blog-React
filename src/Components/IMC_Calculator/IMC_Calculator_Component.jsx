80e0043ae06cf99d3900c50fa1673621794a635e
welinsonmacedo <welinsonmarlon15@gmail.com>
2024-08-23 21:02:47 -0300

    3.2

--- src/Components/IMC_Calculator/IMC_Calculator_Component.jsx
+++ src/Components/IMC_Calculator/IMC_Calculator_Component.jsx
@@ -76,6 +76,7 @@ const Button = styled.button`
 
 const ContainerMessage = styled.div`
   display: flex;
+  flex-wrap: wrap;
   gap: 20px;
   align-items: center;
   justify-content: center;
@@ -95,12 +96,26 @@ const Message = styled.p`
   color: #e63946;
   font-size: 18px;
 `;
+const Description = styled.p`
+  text-align: center;
+  font-weight: bold;
+  color: #3992e6;
+  font-size: 18px;
+`;
+const Recomendation = styled.p`
+  text-align: center;
+  font-weight: bold;
+  color: #96ce2d;
+  font-size: 18px;
+`;
 
 const IMCCalculatorComponent = () => {
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
   const [imc, setImc] = useState(null);
   const [mensagem, setMensagem] = useState('');
+  const [description, setDescription] = useState('');
+  const [recomendation, setRecomendation] = useState('');
 
   const calcularIMC = () => {
     if (peso && altura) {
@@ -120,12 +135,20 @@ const IMCCalculatorComponent = () => {
   const definirMensagem = (imc) => {
     if (imc < 18.5) {
       setMensagem('Abaixo do peso');
+      setDescription('Descrição: Se o IMC for inferior a 18.5, a pessoa é considerada abaixo do peso. Isso pode indicar que ela pode estar subnutrida ou ter problemas de saúde relacionados à desnutrição.');
+      setRecomendation('Recomendações: Em casos de baixo peso, é aconselhável consultar um profissional de saúde para avaliar a dieta, o estilo de vida e outras condições subjacentes.');
     } else if (imc >= 18.5 && imc < 24.9) {
       setMensagem('Peso normal');
+      setDescription('Descrição: Um IMC nessa faixa indica que a pessoa tem um peso considerado saudável para sua altura. Isso geralmente está associado a um menor risco de doenças relacionadas ao peso.');
+      setRecomendation('Recomendações: Manter hábitos saudáveis de alimentação e atividade física é importante para continuar dentro dessa faixa.');
     } else if (imc >= 25 && imc < 29.9) {
       setMensagem('Sobrepeso');
+      setDescription('Descrição: Um IMC entre 25 e 29.9 indica que a pessoa está com sobrepeso. Embora isso não seja necessariamente indicativo de problemas de saúde, pode aumentar o risco de doenças como hipertensão, diabetes tipo 2 e doenças cardíacas.');
+      setRecomendation('Recomendações: Pode ser útil adotar uma alimentação equilibrada e aumentar a atividade física para evitar possíveis complicações de saúde.');
     } else {
       setMensagem('Obesidade');
+      setDescription('Descrição: Um IMC de 30 ou mais indica obesidade. A obesidade é um fator de risco significativo para várias condições graves de saúde, incluindo doenças cardiovasculares, diabetes, apneia do sono, e certos tipos de câncer.');
+      setRecomendation('Recomendações: É importante procurar orientação de um profissional de saúde para criar um plano de ação que inclua dieta, exercício e, em alguns casos, intervenções médicas.');
     }
   };
 
@@ -154,6 +177,8 @@ const IMCCalculatorComponent = () => {
           <ContainerMessage>
             <MessageIMC>{imc}</MessageIMC>
             <Message>{mensagem}</Message>
+            <Description>{description}</Description>
+            <Recomendation>{recomendation}</Recomendation>
           </ContainerMessage>
         )}
       </ContainerCalculator>
